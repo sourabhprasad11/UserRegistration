0683a94ae3cd3f205c35f1ac909d733a868c1092
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-10-15 14:47:15 +0530

    Pattern matches for first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,21 @@
 #! /bin/bash
 echo "Welcome to the User Registration"
+
+function firstname(){
+
+pattern="^([A-Z]){1}([a-zA-Z]+){2,}$"
+read -p "Enter First Name: " x
+
+if [[ $x =~ $pattern ]]; then
+	echo "Pattern matches"
+else
+	echo "Error"
+fi
+}
+
+function main(){
+
+	firstname
+
+}
+main
